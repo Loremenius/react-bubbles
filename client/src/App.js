33c494f88e8ea4de609bf0bcf0703976a82dbb40
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-11-15 10:16:43 -0700

    created functionality for the login page. if successful, saves token to local storage and sends user to bubbles page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
+
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+
+        <PrivateRoute path="/bubbles">
+          <BubblePage/>
+        </PrivateRoute>
       </div>
     </Router>
   );
